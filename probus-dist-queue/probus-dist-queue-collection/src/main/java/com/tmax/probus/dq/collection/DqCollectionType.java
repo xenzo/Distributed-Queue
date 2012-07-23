24f9e517eafa935c33ad6a5fce9d5198c1273e36
Kim Dong iL <kim.same@gmail.com>
2012-07-24 01:09:06 +0900

    proxy

---
+++ probus-dist-queue/probus-dist-queue-collection/src/main/java/com/tmax/probus/dq/collection/DqCollectionType.java
@@ -0,0 +1,35 @@
+/*
+ * DqCollectionType.java Version 1.0 Jul 23, 2012
+ * *
+ * Copyright (c) 2010 by Tmax Soft co., Ltd.
+ * All rights reserved.
+ * *
+ * This software is the confidential and proprietary information of
+ * Tmax Soft co.,Ltd("Confidential Information").
+ * You shall not disclose such Confidential Information
+ * and shall use it only in accordance with the terms of the license agreement
+ * entered into with Tmax Soft co., Ltd.
+ */
+package com.tmax.probus.dq.collection;
+
+/**
+ *
+ */
+public enum DqCollectionType {
+    BlockingQueue(java.util.concurrent.BlockingQueue.class),
+    BlockingDeque(java.util.concurrent.BlockingDeque.class),
+    Stack(java.util.Stack.class),
+    Map(java.util.Map.class),
+    ConcurrentMap(java.util.concurrent.ConcurrentMap.class),
+    ConcurrentNavigableMap(java.util.concurrent.ConcurrentNavigableMap.class),
+    SolidOperator(IDqSolidOperator.class);
+    private DqCollectionType(Class<?> claz) {
+        claz_ = claz;
+    }
+
+    private Class<?> claz_;
+
+    public Class<?> getClazz() {
+        return claz_;
+    }
+}
